955d092c5ae4ab95b684ca88c7ad39a247c05c3c
jhonperez95 <jhonperez.j95@gmail.com>
2021-11-24 11:23:38 -0500

    iniciando

---
+++ src/matrices/Matrices.java
@@ -0,0 +1,14 @@
+
+package matrices;
+
+
+public class Matrices {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
